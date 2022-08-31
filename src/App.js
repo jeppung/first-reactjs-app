6a5ca7c7b3058f7343b485e043d68724a9b36f42
jeppung <jeppung@gmail.com>
2022-09-01 00:43:50 +0700

    Applying search function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ const API_URL = 'https://reqres.in/api/users?page=2';
 const App = () => {
   
   const [users, setUsers] = useState([]);
+  const [input, setInput] = useState('test');
+  const [clicked, setClicked] =  useState(false);
+  const [searchData, setSearchData] = useState();
 
   useEffect(() => {
     const getUsers = async () => {
@@ -21,26 +24,53 @@ const App = () => {
     getUsers();
   }, []);
 
+  const search = async (input) => {
+      const arr = [];
+
+      users.map((x) => {
+        var fullName = x.first_name + " " + x.last_name;
+        if(fullName.toLowerCase().includes(input.toLowerCase())){
+          arr.push(x)
+        }
+      });
+
+      setSearchData(arr);
+      setClicked(true);
+  }
+  
 
   return (
     <div className='app'>
       <h1>UserLand</h1>
       <div className='search'>
-        <input placeholder='Search for users' onChange={() => {}}/>
-        <img src={SearchIcon} alt="search" onClick={() => {}}/>
+        <input placeholder='Search for users' onChange={(x) => setInput(x.target.value)} onEnter/>
+        <img src={SearchIcon} alt="search" onClick={() => search(input)}/>
       </div>
 
       <div className='container'>
         {
-          users.length > 0 ?
-          users.map((user) => (
-            <UserCard user={user}/>
-          )):
-          (
-            <div>
-              <h2>No Data</h2>
-            </div>
-          )
+          clicked ? 
+            searchData.length > 0 ?
+            searchData.map((user) => (
+              <UserCard user={user}/>
+            ))
+            :
+            (
+              <div>
+                <h2>No Data</h2>
+                {setClicked(false)}
+              </div>
+            )
+          :
+            users.length > 0 ?
+            users.map((user) => (
+              <UserCard user={user}/>
+            )):
+            (
+              <div>
+                <h2>No Data</h2>
+              </div>
+            )
         }
       </div>  
     </div>
@@ -48,3 +78,6 @@ const App = () => {
 }
 
 export default App;
+
+
+
